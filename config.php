072f6b3bf457d940fef4426424c1cad1408945ed
Andrew <101130154+AndrewCieslik@users.noreply.github.com>
2024-03-04 23:51:14 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('_SERVER_NAME', 'localhost');
-define('_SERVER_URL', 'http://' . _SERVER_NAME);
-define('_APP_ROOT', '/credit_calc');
-define('_APP_URL', _SERVER_URL . _APP_ROOT);
-define("_ROOT_PATH", __DIR__);
-define('_BACKGROUND_IMAGE_URL', _APP_URL . '/images/background.jpg');
-
