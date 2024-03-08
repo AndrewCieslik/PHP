f5f196750882b86a59cb8ab18a4c91d949d49a64
Andrew <cieslik.andrew@gmail.com>
2024-03-08 21:12:01 +0100

    erase header in index

--- 2_CreditCalculator_Secured/index.php
+++ 2_CreditCalculator_Secured/index.php
@@ -1,8 +1,4 @@
 <?php
 require_once __DIR__.'/config.php';
 
-//przekierowanie przeglądarki klienta (redirect)
-//header("Location: "._APP_URL."/app/calc.php");
-
-//przekazanie żądania do następnego dokumentu ("forward")
 include _ROOT_PATH.'/app/calc.php';
