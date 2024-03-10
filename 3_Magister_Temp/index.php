89400336064edc35525fb2f9f9c56df3f0cf184f
Andrew <cieslik.andrew@gmail.com>
2024-03-10 07:52:36 +0100

    view_menu

--- 3_Magister_Temp/index.php
+++ 3_Magister_Temp/index.php
@@ -31,20 +31,10 @@
     require_once 'config.php';
 ?>
 
-<nav class="mainmenu">
-	<div class="container">
-		<div class="dropdown">
-			<button type="button" class="navbar-toggle" data-toggle="dropdown"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-			<!-- <a data-toggle="dropdown" href="#">Dropdown trigger</a> -->
-			<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-				<li><a href="#head" class="active">Wstep</a></li>
-				<li><a href="#about">O aplikacji</a></li>
-				<li><a href="#themes">Oblicz rate kredytu</a></li>
-				<li><a href="#contact">Kontakt</a></li>
-			</ul>
-		</div>
-	</div>
-</nav>
+<?php
+    include "views/view_menu.php";
+?>
+
 
 
 <!-- Main (Home) section -->
--- 3_Magister_Temp/views/view_footer.php
+++ 3_Magister_Temp/views/view_footer.php
@@ -2,6 +2,6 @@
 <<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
 	<p class="tagline">
 		<i>Aplikacja testowa na potrzeby laboratorium - Stopka</i></br>
-		<b>template designed by GetTemplate</b>
+		<b>template designed by Sergey Pozhilov (GetTemplate.com)</b>
     </p>	
 </div> 
