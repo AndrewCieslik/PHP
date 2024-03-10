b04f2f3c58331bcbb6c1b92c1998d45b01d69ba7
Andrew <cieslik.andrew@gmail.com>
2024-03-10 08:08:58 +0100

    project 4 + include smarty

---
+++ 4_Magister_Temp_Smarty/views/view_footer.php
@@ -0,0 +1,7 @@
+
+<<div class="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">	
+	<p class="tagline">
+		<i>Aplikacja testowa na potrzeby laboratorium - Stopka</i></br>
+		<b>template designed by Sergey Pozhilov (GetTemplate.com)</b>
+    </p>	
+</div> 
