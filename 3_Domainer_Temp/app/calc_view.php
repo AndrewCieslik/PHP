e86e8afd163d790afbb619f6b39c874a5e22b016
Andrew <cieslik.andrew@gmail.com>
2024-03-14 09:36:50 +0100

    button counter

--- 3_Domainer_Temp/app/calc_view.php
+++ 3_Domainer_Temp/app/calc_view.php
@@ -44,21 +44,27 @@
 		<div style="width:90%; margin: 2em auto;">
 
 			<form class="pure-form pure-form-stacked" action="<?php print(_APP_URL); ?>/app/calc.php" method="post">
-					<label for="id_credit">Kwota kredytu: </label>
+					<label for="id_credit"><h1>Credit: </h1></label>
+					
+					<div class="col span_12">
+                   		 <input type="text" placeholder="Your Name" id="f_name" name="f_name">
+						 
+                	</div>
 					<!-- php out(46min 03lab-->
-					<input id="id_credit" type="number" step="any" name="credit" min="1" required value="<?php if (isset($credit))
-						print($credit); ?>" /><br />
+					<input class="align-center margin" id="id_credit" type="number" step="any" name="credit" min="1" required value="<?php out($credit); ?>" /><br />
 			
-					<label for="id_percent">Oprocentowanie: </label>
-					<input id="id_percent" type="number" step="any" name="percent" min="0" required value="<?php if (isset($percent))
-						print($percent); ?>" /><br />
+					<label for="id_percent"><h1>Percent: </h1></label>
+					<input class="align-center margin" id="id_percent" type="number" step="any" name="percent" min="0" required value="<?php out($percent); ?>" /><br />
 			
-					<label for="id_years">Liczba lat kredytu: </label>
-					<input id="id_years" type="number" name="years" min="1" required value="<?php if (isset($years))
-						print($years); ?>" /><br />
-					<input class="pure-button pure-button-primary" type="submit" value="Oblicz miesięczną ratę" />
+					<label for="id_years"><h1>Number of years: </h1></label>
+					<input class="align-center margin" id="id_years" type="number" name="years" min="1" required value="<?php out($years); ?>" /><br /></br>
+					<div class="col span_8">
+						<input class="btn btn-icon btn-block"  value="Count monthly loan" type="submit"><i class="icon icon-envelope"></i>
+					</div>
 			</form>
 			
+			</br></br></br>
+
 			<?php
 			if (isset($messages)) {
 				if (count ( $messages ) > 0) {
@@ -73,96 +79,84 @@
 			
 			<?php if (isset($result)): ?>
 				<div>
-					<p>Wartość miesięcznej raty: <span class="result-value"><?php echo $result; ?> zł</span></p>
+					<h2 class ="box">The value of the monthly loan: <span class="result-value"><?php echo $result; ?> zł</span></h2>
 				</div>
 			<?php endif; ?>
 		</div>
 			
-		<h2 class="align-center margin">Reasons to buy MegaDomain.com</h2>
-		<div class="row padding">
-			<div class="col span_8">
-				<div class="circle"><i class="icon icon-support"></i></div>
-				<h3 class="align-center">Easy to remember domain</h3>
-				<p class="align-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh 
-					euismod tincidunt ut laoreet dolore magna.</p>
-			</div>
+		<h2 class="align-center margin">Reasons to Use CreditCalculator</h2>
+<div class="row padding">
+    <div class="col span_8">
+        <div class="circle"><i class="icon icon-support"></i></div>
+        <h3 class="align-center">Simple Loan Calculation</h3>
+        <p class="align-center">Planning your finances is crucial. CreditCalculator offers a user-friendly loan calculator to help you estimate your monthly payments.</p>
+    </div>
 
-			<div class="col span_8">
-				<div class="circle"><i class="icon icon-stockup"></i></div>
-				<h3 class="align-center">Awesome stats</h3>
-				<p class="align-center">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, 
-					vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio.</p>
-			</div>
+    <div class="col span_8">
+        <div class="circle"><i class="icon icon-stockup"></i></div>
+        <h3 class="align-center">Accurate Results</h3>
+        <p class="align-center">Our loan calculator provides precise estimates based on your inputs, ensuring you have a clear understanding of your financial obligations.</p>
+    </div>
 
-			<div class="col span_8">
-				<div class="circle"><i class="icon icon-briefcase"></i></div>
-				<h3 class="align-center">Popularity</h3>
-				<p class="align-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
-					tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.</p>
-			</div>
-		</div> <!-- end of row -->
-		
-		<hr class="divider">
+    <div class="col span_8">
+        <div class="circle"><i class="icon icon-briefcase"></i></div>
+        <h3 class="align-center">Customizable Options</h3>
+        <p class="align-center">Tailor your loan parameters to suit your needs. CreditCalculator empowers you to adjust variables such as loan amount, interest rate, and term.</p>
+    </div>
+</div> <!-- end of row -->
 
-		<div class="row padding">
-			<div class="col span_8">
+<hr class="divider">
 
-				<!-- "About seller" text block. Feel free to replace it by any other text or advertisement -->
-				<h2>About Seller</h2>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh 
-				euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
-				<p>AZ Domains has a variety of domain in many niches. So, if you like one of the domaint listed below, 
-					feel free to make an offer for that domain too. 
-				</p>
-				<p><img src="assets/images/sample_signature.png" alt=""></p>
+<div class="row padding">
+    <div class="col span_8">
+        <h2>About CreditCalculator</h2>
+        <p>At CreditCalculator, we understand the significance of informed financial decisions. Our platform offers comprehensive information about loans and provides valuable tools like our loan calculator to assist you in managing your finances effectively.</p>
+        <p>Discover the potential of smart financial planning with CreditCalculator.</p>
+    </div>
+    <div class="col span_16">
+        <h2>Calculate Your Monthly Loan Payment</h2>
+        <div id="Offer">
+            <div class="row">
+                <div class="col span_12">
+                    <input type="text" placeholder="Your Name" id="f_name" name="f_name">
+                </div>
+                <div class="col span_12">
+                    <input type="text" placeholder="Your email address" id="f_email" name="f_email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col span_24">
+                    <textarea id="f_offer" name="f_offer" rows="10">I would like to calculate my monthly loan payment.</textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col span_24 align-right">
+                    <button class="btn btn-large" type="button">Calculate</button>
+                    <div class="subscribe">
+                        <input type="checkbox" name="f_subscribe" id="f_subscribe" value="yes"><label for="f_subscribe"> <i></i>Subscribe to our newsletter</label>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end of offer form -->
 
-			</div>
-			<div class="col span_16">
+        <!-- Result Messages -->
+        <div class="row" id="error_msg">
+            <div class="col span_24">
+                <b>Sorry, but there were errors found with the form you submitted:
+                    <i></i></b>
+                <br><a href="javascript:void(0);" id="new_try">Try Again</a>
+            </div>
+        </div>
+        <div class="row" id="msg">
+            <div class="col span_24">
+                <b>Calculation Sent!</b>
+                <br><a href="javascript:void(0);" id="new_offer">Calculate Another?</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 
-				<!-- Offer submission form. Please don't change inputs' IDs and names. -->
-				<h2>Make an offer</h2>
-				<div id="Offer">
-					<div class="row">
-						<div class="col span_12">
-							<input type="text" placeholder="Your Name" id="f_name" name="f_name" >
-						</div>
-						<div class="col span_12">
-							<input type="text" placeholder="Your email address" id="f_email" name="f_email" >
-						</div>
-					</div>
-					<div class="row">
-						<div class="col span_24">
-							<textarea id="f_offer" name="f_offer" rows="10">I would like to offer $100 000 for megadomain.com</textarea>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col span_24 align-right">
-							<button class="btn btn-large" type="button">Submit offer</button>
-							<div class="subscribe">
-								<input type="checkbox" name="f_subscribe" id="f_subscribe" value="yes"><label for="f_subscribe"> <i></i>Subscribe to our newsletter</label>
-							</div>
-						</div>
-					</div>
-				</div> <!-- end of offer form -->
-				
-				<!-- Result Messages -->
-				<div class="row" id="error_msg">
-					<div class="col span_24">
-						<b>Sorry, but there were error(s) found with the form you submitted:
-						<i></i></b> 
-						<br><a href="javascript:void(0);" id="new_try">Got it, retry.</a>
-					</div>
-				</div>
-				<div class="row" id="msg">
-					<div class="col span_24">
-						<b>Offer sent!</b> 
-						<br><a href="javascript:void(0);" id="new_offer">Make another one?</a>
-					</div>
-				</div>
-
-			</div> 
-		</div> 
-	</div>
 
 	<div id="Content-end" class="container"></div>
 
