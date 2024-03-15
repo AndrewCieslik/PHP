1fc010cc51c994a8f9604c734fcb6c5660754527
Andrew <cieslik.andrew@gmail.com>
2024-03-15 13:54:12 +0100

    h2/h3

--- 4_Domainer_Temp_Smarty/templates_c/0b5510b7da02b5edca0c668c0739e4fbb0e8b8a4_0.file.calc.tpl.php
+++ 4_Domainer_Temp_Smarty/templates_c/0b5510b7da02b5edca0c668c0739e4fbb0e8b8a4_0.file.calc.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.38, created on 2024-03-15 08:15:44
+/* Smarty version 3.1.38, created on 2024-03-15 13:44:14
   from '/var/www/PHP/4_Domainer_Temp_Smarty/app/calc.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.38',
-  'unifunc' => 'content_65f3f5a03d4a77_45062238',
+  'unifunc' => 'content_65f4429e6fe187_44258108',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0b5510b7da02b5edca0c668c0739e4fbb0e8b8a4' => 
     array (
       0 => '/var/www/PHP/4_Domainer_Temp_Smarty/app/calc.tpl',
-      1 => 1710486938,
+      1 => 1710490758,
       2 => 'file',
     ),
   ),
@@ -20,136 +20,306 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_65f3f5a03d4a77_45062238 (Smarty_Internal_Template $_smarty_tpl) {
+function content_65f4429e6fe187_44258108 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_70115541665f3f5a0395b41_17041689', 'footer');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_181823111965f4429e6f47a7_12167220', 'calc');
 ?>
 
 
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_37028258165f4429e6fd481_68856861', 'content');
+?>
 
-<?php ob_start();
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_34631049765f3f5a0396940_86602861', 'content');
-$_prefixVariable1 = ob_get_clean();
-echo $_prefixVariable1;
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_11562719065f4429e6fdba2_24376089', 'header');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_165141626465f4429e6fdea0_30105005', 'footer');
 $_smarty_tpl->inheritance->endChild($_smarty_tpl, "../templates/main.html");
 }
-/* {block 'footer'} */
-class Block_70115541665f3f5a0395b41_17041689 extends Smarty_Internal_Block
+/* {block 'calc'} */
+class Block_181823111965f4429e6f47a7_12167220 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
-  'footer' => 
+  'calc' => 
   array (
-    0 => 'Block_70115541665f3f5a0395b41_17041689',
-  ),
-);
-public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
-?>
-przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
-}
-}
-/* {/block 'footer'} */
-/* {block 'content'} */
-class Block_34631049765f3f5a0396940_86602861 extends Smarty_Internal_Block
-{
-public $subBlocks = array (
-  'content' => 
-  array (
-    0 => 'Block_34631049765f3f5a0396940_86602861',
+    0 => 'Block_181823111965f4429e6f47a7_12167220',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
     <div style="width:90%; margin: 3em auto;">
-
-    <form class=""  action="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+        <form class="" action="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
 /app/calc.php" method="post">
-        <br />
-        <div class="col span_24">
-            <label for="id_percent"><h1>Credit:</h1></label><br />
-        </div>
+            <br />
+            <div class="col span_24">
+                <label for="id_percent">
+                    <h1>Credit:</h1>
+                </label><br />
+            </div>
 
-        <div class="col span_12">
-               <input type="text" placeholder="Your credit value" id="id_credit" name="credit"/>
-        </div>
+            <div class="col span_12">
+                <input type="text" placeholder="Your credit value" id="id_credit" name="credit" value="<?php if ((isset($_smarty_tpl->tpl_vars['credit']->value))) {
+echo $_smarty_tpl->tpl_vars['credit']->value;
+}?>"/>
+            </div>
 
-        <div class="col span_24">
-            <label for="id_percent"><h1>Percent:</h1></label><br />
-        </div>
+            <div class="col span_24">
+                <label for="id_percent">
+                    <h1>Percent:</h1>
+                </label><br />
+            </div>
 
-        <div class="col span_12">
-               <input type="text" placeholder="Percent value" id="id_percent" name="percent" />
-        </div>
+            <div class="col span_12">
+                <input type="text" placeholder="Percent value" id="id_percent" name="percent" value="<?php if ((isset($_smarty_tpl->tpl_vars['percent']->value))) {
+echo $_smarty_tpl->tpl_vars['percent']->value;
+}?>"/>
+            </div>
 
-        <div class="col span_24">
-            <label for="id_years"><h1>Number of years: </h1></label><br />
-        </div>
-        
-        <div class="col span_12">
-            <input type="text" placeholder="Credit lenght in years" id="id_years" name="years"/>
-        </div>
-        
-        <div class="col span_24">
-            <input class="btn btn-icon btn-block"  value="Count monthly loan" type="submit"><br />
-            <div class="messages">
+            <div class="col span_24">
+                <label for="id_years">
+                    <h1>Number of years: </h1>
+                </label><br />
+            </div>
 
-<?php if ((isset($_smarty_tpl->tpl_vars['messages']->value))) {?>
-	<?php if (count($_smarty_tpl->tpl_vars['messages']->value) > 0) {?> 
-		<h4>Wystąpiły błędy: </h4>
-		<ol class="err">
-		<?php
+            <div class="col span_12">
+                <input type="text" placeholder="Credit lenght in years" id="id_years" name="years" value="<?php if ((isset($_smarty_tpl->tpl_vars['years']->value))) {
+echo $_smarty_tpl->tpl_vars['years']->value;
+}?>"/>
+            </div>
+
+            <div class="col span_24">
+                <input class="btn btn-icon btn-block" value="Count monthly loan" type="submit"><br />
+
+                <?php if ((isset($_smarty_tpl->tpl_vars['messages']->value))) {?>
+                    <?php if (count($_smarty_tpl->tpl_vars['messages']->value) > 0) {?>
+                        <h3>Error: </h3>
+                        <ol class="err">
+                            <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['messages']->value, 'msg');
 $_smarty_tpl->tpl_vars['msg']->do_else = true;
 if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
 $_smarty_tpl->tpl_vars['msg']->do_else = false;
 ?>
-		<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+                                <li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
 </li>
-		<?php
+                            <?php
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
-		</ol>
-	<?php }
-}?>
+                        </ol>
+                    <?php }?>
+                <?php }?>
 
-<?php if ((isset($_smarty_tpl->tpl_vars['infos']->value))) {?>
-	<?php if (count($_smarty_tpl->tpl_vars['infos']->value) > 0) {?> 
-		<h4>Informacje: </h4>
-		<ol class="inf">
-		<?php
+                <?php if ((isset($_smarty_tpl->tpl_vars['infos']->value))) {?>
+                    <?php if (count($_smarty_tpl->tpl_vars['infos']->value) > 0) {?>
+                        <h3>Information: </h3>
+                        <ol class="inf">
+                            <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['infos']->value, 'msg');
 $_smarty_tpl->tpl_vars['msg']->do_else = true;
 if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['msg']->value) {
 $_smarty_tpl->tpl_vars['msg']->do_else = false;
 ?>
-		<li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
+                                <li><?php echo $_smarty_tpl->tpl_vars['msg']->value;?>
 </li>
-		<?php
+                            <?php
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
-		</ol>
-	<?php }
-}?>
+                        </ol>
+                    <?php }?>
+                <?php }?>
 
-<?php if ((isset($_smarty_tpl->tpl_vars['result']->value))) {?>
-	<h4>Wynik</h4>
-	<p class="res">
-	<?php echo $_smarty_tpl->tpl_vars['result']->value;?>
+                <?php if ((isset($_smarty_tpl->tpl_vars['result']->value))) {?>
+                    <h2>Result:</h2>
+                    <p class="res">
+                    <h1><?php echo $_smarty_tpl->tpl_vars['result']->value;?>
+ zl</h1>
+                    </p>
+                <?php }?>
+            </div>
+        </form>
+    </div>
+<?php
+}
+}
+/* {/block 'calc'} */
+/* {block 'calc'} */
+class Block_61415687565f4429e6fd681_97169323 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+}
+}
+/* {/block 'calc'} */
+/* {block 'content'} */
+class Block_37028258165f4429e6fd481_68856861 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_37028258165f4429e6fd481_68856861',
+  ),
+  'calc' => 
+  array (
+    0 => 'Block_61415687565f4429e6fd681_97169323',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
 
-	</p>
-<?php }?>
+    <div id="Content" class="container">
 
-</div>
+        <div class="row special">
+            <div class="col span_24">
+                <h3 class="align-center">Before proceeding with your loan application, please be aware that taking a loan
+                    involves financial risks and careful consideration of repayment terms.</h3>
+            </div>
         </div>
-    </form>
-</div>	
+        <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_61415687565f4429e6fd681_97169323', 'calc', $this->tplIndex);
+?>
+
+
+        <div class="row padding">
+        </div>
+
+        <hr class="divider">
+
+        <div class="row padding">
+
+            <div class="col span_8">
+                <div class="circle"><i class="icon icon-support"></i></div>
+                <h3 class="align-center">Simple Loan Calculation</h3>
+                <p class="align-center">Planning your finances is crucial. CreditCalculator offers a user-friendly loan
+                    calculator to help you estimate your monthly payments.</p>
+            </div>
+
+            <div class="col span_8">
+                <div class="circle"><i class="icon icon-stockup"></i></div>
+                <h3 class="align-center">Accurate Results</h3>
+                <p class="align-center">Our loan calculator provides precise estimates based on your inputs, ensuring you
+                    have a clear understanding of your financial obligations.</p>
+            </div>
+
+            <div class="col span_8">
+                <div class="circle"><i class="icon icon-briefcase"></i></div>
+                <h3 class="align-center">Customizable Options</h3>
+                <p class="align-center">Tailor your loan parameters to suit your needs. CreditCalculator empowers you to
+                    adjust variables such as loan amount, interest rate, and term.</p>
+            </div>
+
+            <div class="row padding">
+
+                <div class="col span_8">
+                    <h2>About CreditCalculator</h2>
+                    <p>At CreditCalculator, we understand the significance of informed financial decisions. Our platform
+                        offers comprehensive information about loans and provides valuable tools like our loan calculator to
+                        assist you in managing your finances effectively.</p>
+                    <p>Discover the potential of smart financial planning with CreditCalculator.</p>
+                </div>
+                <div class="col span_16">
+                    <h2>Calculate Your Monthly Loan Payment</h2>
+                    <div id="Offer">
+                        <div class="row">
+                            <div class="col span_12">
+                                <input type="text" placeholder="Your Name" id="f_name" name="f_name">
+                            </div>
+                            <div class="col span_12">
+                                <input type="text" placeholder="Your email address" id="f_email" name="f_email">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col span_24">
+                                <textarea id="f_offer" name="f_offer"
+                                    rows="10">I would like to calculate my monthly loan payment.</textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col span_24 align-right">
+                                <button class="btn btn-large" type="button">Calculate</button>
+                                <div class="subscribe">
+                                    <input type="checkbox" name="f_subscribe" id="f_subscribe" value="yes"><label
+                                        for="f_subscribe"> <i></i>Subscribe to our newsletter</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- end of offer form -->
+
+                    <!-- Result Messages -->
+                    <div class="row" id="error_msg">
+                        <div class="col span_24">
+                            <b>Sorry, but there were errors found with the form you submitted:
+                                <i></i></b>
+                            <br><a href="javascript:void(0);" id="new_try">Try Again</a>
+                        </div>
+                    </div>
+                    <div class="row" id="msg">
+                        <div class="col span_24">
+                            <b>Calculation Sent!</b>
+                            <br><a href="javascript:void(0);" id="new_offer">Calculate Another?</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="Content-end" class="container"></div>
 <?php
 }
 }
 /* {/block 'content'} */
+/* {block 'header'} */
+class Block_11562719065f4429e6fdba2_24376089 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'header' => 
+  array (
+    0 => 'Block_11562719065f4429e6fdba2_24376089',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+    <div id="Head" class="container">
+        <div class="row">
+            <div class="col span_16">
+                <h1 id="Domain">Credit Calculator<br>
+                    <span>Free online calculator for your credit</span>
+                </h1>
+            </div>
+        </div>
+    </div>
+<?php
+}
+}
+/* {/block 'header'} */
+/* {block 'footer'} */
+class Block_165141626465f4429e6fdea0_30105005 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_165141626465f4429e6fdea0_30105005',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+        <div id="Footer" class="container">
+            <div class="row top">
+                <div class="col span_16">Copyright &copy; 2014, Domain holder. All rights reserved.</div>
+                <div class="col span_8 align-right">Design: <a href="http://www.gettemplate.com/">GetTemplate</a></div>
+            </div>
+        </div>
+<?php
+}
+}
+/* {/block 'footer'} */
 }
--- 4_Domainer_Temp_Smarty/templates_c/db6c83a9b042f81ae62e08c4615d01d62d04d08e_0.file.main.html.php
+++ 4_Domainer_Temp_Smarty/templates_c/db6c83a9b042f81ae62e08c4615d01d62d04d08e_0.file.main.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.38, created on 2024-03-15 08:08:59
+/* Smarty version 3.1.38, created on 2024-03-15 13:44:14
   from '/var/www/PHP/4_Domainer_Temp_Smarty/templates/main.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.38',
-  'unifunc' => 'content_65f3f40be5a2b7_81041730',
+  'unifunc' => 'content_65f4429e70a733_56072419',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'db6c83a9b042f81ae62e08c4615d01d62d04d08e' => 
     array (
       0 => '/var/www/PHP/4_Domainer_Temp_Smarty/templates/main.html',
-      1 => 1710486524,
+      1 => 1710489795,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_65f3f40be5a2b7_81041730 (Smarty_Internal_Template $_smarty_tpl) {
+function content_65f4429e70a733_56072419 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 ?>
@@ -51,113 +51,20 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 /assets/images/apple-touch-icon-114x114.png">
 </head>
 <body>
-	<div id="Head" class="container">
-		<div class="row">
-			<div class="col span_16">
-				<h1 id="Domain">Credit Calculator<br>
-					<span>Free online calculator for your credit</span></h1>
-			</div>
-		</div>
-	</div>
-	
-	<div id="Content" class="container">
-	
-		<div class="row special">
-			<div class="col span_24">
-				<h3 class="align-center">Before proceeding with your loan application, please be aware that taking a loan involves financial risks and careful consideration of repayment terms.</h3>
-			</div>
-		</div>
-		<?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_105989772165f3f40be595f2_89016080', 'content');
+	<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_85556008865f4429e709db5_46337782', 'header');
 ?>
 
 
-	<!--content-->	
-	<div class="row padding">
-</div> 
-
-<hr class="divider">
-
-<div class="row padding">
-
-<div class="col span_8">
-        <div class="circle"><i class="icon icon-support"></i></div>
-        <h3 class="align-center">Simple Loan Calculation</h3>
-        <p class="align-center">Planning your finances is crucial. CreditCalculator offers a user-friendly loan calculator to help you estimate your monthly payments.</p>
-    </div>
-
-    <div class="col span_8">
-        <div class="circle"><i class="icon icon-stockup"></i></div>
-        <h3 class="align-center">Accurate Results</h3>
-        <p class="align-center">Our loan calculator provides precise estimates based on your inputs, ensuring you have a clear understanding of your financial obligations.</p>
-    </div>
-
-    <div class="col span_8">
-        <div class="circle"><i class="icon icon-briefcase"></i></div>
-        <h3 class="align-center">Customizable Options</h3>
-        <p class="align-center">Tailor your loan parameters to suit your needs. CreditCalculator empowers you to adjust variables such as loan amount, interest rate, and term.</p>
-    </div>
-
-<div class="row padding">
-
-    <div class="col span_8">
-        <h2>About CreditCalculator</h2>
-        <p>At CreditCalculator, we understand the significance of informed financial decisions. Our platform offers comprehensive information about loans and provides valuable tools like our loan calculator to assist you in managing your finances effectively.</p>
-        <p>Discover the potential of smart financial planning with CreditCalculator.</p>
-    </div>
-    <div class="col span_16">
-        <h2>Calculate Your Monthly Loan Payment</h2>
-        <div id="Offer">
-            <div class="row">
-                <div class="col span_12">
-                    <input type="text" placeholder="Your Name" id="f_name" name="f_name">
-                </div>
-                <div class="col span_12">
-                    <input type="text" placeholder="Your email address" id="f_email" name="f_email">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col span_24">
-                    <textarea id="f_offer" name="f_offer" rows="10">I would like to calculate my monthly loan payment.</textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col span_24 align-right">
-                    <button class="btn btn-large" type="button">Calculate</button>
-                    <div class="subscribe">
-                        <input type="checkbox" name="f_subscribe" id="f_subscribe" value="yes"><label for="f_subscribe"> <i></i>Subscribe to our newsletter</label>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- end of offer form -->
-
-        <!-- Result Messages -->
-        <div class="row" id="error_msg">
-            <div class="col span_24">
-                <b>Sorry, but there were errors found with the form you submitted:
-                    <i></i></b>
-                <br><a href="javascript:void(0);" id="new_try">Try Again</a>
-            </div>
-        </div>
-        <div class="row" id="msg">
-            <div class="col span_24">
-                <b>Calculation Sent!</b>
-                <br><a href="javascript:void(0);" id="new_offer">Calculate Another?</a>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
+	<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_71507530065f4429e70a174_26719520', 'content');
+?>
 
 
-	<div id="Content-end" class="container"></div>
+	<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_118197198465f4429e70a447_51421970', 'footer');
+?>
 
-	<div id="Footer" class="container">
-		<div class="row top">
-			<div class="col span_16">Copyright &copy; 2014, Domain holder. All rights reserved.</div>
-			<div class="col span_8 align-right">Design: <a href="http://www.gettemplate.com/">GetTemplate</a></div>
-		</div>
-	</div>
 
 <?php echo '<script'; ?>
  src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><?php echo '</script'; ?>
@@ -168,20 +75,43 @@ $_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_105989772165f3f40b
 
 </body>
 </html><?php }
+/* {block 'header'} */
+class Block_85556008865f4429e709db5_46337782 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'header' => 
+  array (
+    0 => 'Block_85556008865f4429e709db5_46337782',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+}
+}
+/* {/block 'header'} */
 /* {block 'content'} */
-class Block_105989772165f3f40be595f2_89016080 extends Smarty_Internal_Block
+class Block_71507530065f4429e70a174_26719520 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_105989772165f3f40be595f2_89016080',
+    0 => 'Block_71507530065f4429e70a174_26719520',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
-?>
- 
-		<?php
 }
 }
 /* {/block 'content'} */
+/* {block 'footer'} */
+class Block_118197198465f4429e70a447_51421970 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_118197198465f4429e70a447_51421970',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+}
+}
+/* {/block 'footer'} */
 }
