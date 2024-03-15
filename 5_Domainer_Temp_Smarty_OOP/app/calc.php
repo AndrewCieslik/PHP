fe6256564801ba4e01330af2c922232565084404
Andrew <cieslik.andrew@gmail.com>
2024-03-15 14:21:32 +0100

    require smarty

--- 5_Domainer_Temp_Smarty_OOP/app/calc.php
+++ 5_Domainer_Temp_Smarty_OOP/app/calc.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__FILE__).'/../config.php';
+require_once $conf->root_path. '/lib/smarty/Smarty.class.php';
 
 //załaduj kontroler
 require_once $conf->root_path.'/app/CalcCtrl.class.php';
