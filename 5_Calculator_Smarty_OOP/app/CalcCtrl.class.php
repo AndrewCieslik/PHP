ab7dbcdb9b021592cfe75af0d92854ff9c23fad4
Andrew <cieslik.andrew@gmail.com>
2024-03-15 11:33:51 +0100

    comments

--- 5_Calculator_Smarty_OOP/app/CalcCtrl.class.php
+++ 5_Calculator_Smarty_OOP/app/CalcCtrl.class.php
@@ -1,134 +1,79 @@
 <?php
-// W skrypcie definicji kontrolera nie trzeba dołączać problematycznego skryptu config.php,
-// ponieważ będzie on użyty w miejscach, gdzie config.php zostanie już wywołany.
-
 require_once $conf->root_path.'/lib/smarty/Smarty.class.php';
 require_once $conf->root_path.'/lib/Messages.class.php';
 require_once $conf->root_path.'/app/CalcForm.class.php';
 require_once $conf->root_path.'/app/CalcResult.class.php';
 
-/** Kontroler kalkulatora
- * @author Przemysław Kudłacik
- *
- */
 class CalcCtrl {
 
-	private $msgs;   //wiadomości dla widoku
-	private $form;   //dane formularza (do obliczeń i dla widoku)
-	private $result; //inne dane dla widoku
+	private $msgs;   
+	private $form;   
+	private $result; 
 
-	/** 
-	 * Konstruktor - inicjalizacja właściwości
-	 */
 	public function __construct(){
-		//stworzenie potrzebnych obiektów
 		$this->msgs = new Messages();
 		$this->form = new CalcForm();
 		$this->result = new CalcResult();
 	}
-	
-	/** 
-	 * Pobranie parametrów
-	 */
+
 	public function getParams(){
 		$this->form->credit = isset($_REQUEST ['credit']) ? $_REQUEST ['credit'] : null;
 		$this->form->percent = isset($_REQUEST ['percent']) ? $_REQUEST ['percent'] : null;
 		$this->form->years = isset($_REQUEST ['years']) ? $_REQUEST ['years'] : null;
 	}
-	
-	/** 
-	 * Walidacja parametrów
-	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
-	 */
+
 	public function validate() {
-		// sprawdzenie, czy parametry zostały przekazane
-		if (! (isset ( $this->form->credit ) && isset ( $this->form->percent ) && isset ( $this->form->years ))) {
-			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-			return false; //zakończ walidację z błędem
+		if (!(isset($this->form->credit) && isset($this->form->percent) && isset($this->form->years))) {
+			return false; 
 		}
-		
-		// sprawdzenie, czy potrzebne wartości zostały przekazane
 		if ($this->form->credit == "") {
-			$this->msgs->addError('Nie podano liczby 1');
+			$this->msgs->addError('Number 1 not provided');
 		}
 		if ($this->form->percent == "") {
-			$this->msgs->addError('Nie podano liczby 2');
+			$this->msgs->addError('Number 2 not provided');
 		}
 		if ($this->form->years == "") {
-			$this->msgs->addError('Nie podano liczby 3');
+			$this->msgs->addError('Number 3 not provided');
 		}
 		
-		// nie ma sensu walidować dalej gdy brak parametrów
-		if (! $this->msgs->isError()) {
+		if (!$this->msgs->isError()) {
 			
-			// sprawdzenie, czy $x i $y są liczbami całkowitymi
-			if (! is_numeric ( $this->form->credit )) {
-				$this->msgs->addError('Pierwsza wartość nie jest liczbą całkowitą');
+			if (!is_numeric($this->form->credit)) {
+				$this->msgs->addError('First value is not a whole number');
 			}
 			
-			if (! is_numeric ( $this->form->percent )) {
-				$this->msgs->addError('Druga wartość nie jest liczbą całkowitą');
+			if (!is_numeric($this->form->percent)) {
+				$this->msgs->addError('Second value is not a whole number');
 			}
-
-			if (! is_numeric ( $this->form->years )) {
-				$this->msgs->addError('Trzecia wartość nie jest liczbą całkowitą');
-			}
-		}
-		
-		return ! $this->msgs->isError();
-	}
 	
-	/** 
-	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
-	 */
+			if (!is_numeric($this->form->years)) {
+				$this->msgs->addError('Third value is not a whole number');
+			}
+		}
+		return !$this->msgs->isError();
+	}	
+	
 	public function process(){
 
 		$this->getparams();
 		
 		if ($this->validate()) {
-				
-			//konwersja parametrów na int
+		
 			$this->form->credit = (float)($this->form->credit);
 			$this->form->percent = (float)($this->form->percent);
 			$this->form->years = (int)($this->form->years);
-			$this->msgs->addInfo('Parametry poprawne.');
+			$this->msgs->addInfo('Correct parameters.');
 				
-			// //wykonanie operacji
-			// switch ($this->form->op) {
-			// 	case 'minus' :
-			 		// $this->result->result = $this->form->credit - $this->form->percent;
-			// 		$this->result->op_name = '-';
-			// 		break;
-			// 	case 'times' :
-			// 		$this->result->result = $this->form->x * $this->form->y;
-			// 		$this->result->op_name = '*';
-			// 		break;
-			// 	case 'div' :
-			// 		$this->result->result = $this->form->x / $this->form->y;
-			// 		$this->result->op_name = '/';
-			// 		break;
-			// 	default :
-			// 		$this->result->result = $this->form->x + $this->form->y;
-			// 		$this->result->op_name = '+';
-			// 		break;
-			// }
 			$this->result->result = ($this->form->credit + ($this->form->percent * $this->form->credit / 100)) / ($this->form->years * 12);
     		$this->result->result = number_format($this->result->result, 2, '.', '');
 			
-			$this->msgs->addInfo('Wykonano obliczenia.');
+			$this->msgs->addInfo('Correct.');
 		}
 		global $role;
 	
-	
-	
-		
 		$this->generateView();
 	}
 	
-	
-	/**
-	 * Wygenerowanie widoku
-	 */
 	public function generateView(){
 		global $conf;
 		
