8f9846a288829cecd696b9da2fe362f2d0da4370
Andrew <cieslik.andrew@gmail.com>
2024-03-16 20:46:32 +0100

    temps

---
+++ 6_Calculator_Smarty_OOP_action/app/templates_c/238cb204e16af550aae53aa6789f86f94ab5ec5c_0.file.CalcView.html.php
@@ -0,0 +1,129 @@
+<?php
+/* Smarty version 3.1.30, created on 2024-03-16 20:34:50
+  from "/var/www/PHP/6_Calculator_Smarty_OOP_action/app/calc/CalcView.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_65f5f45a471e98_58952049',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '238cb204e16af550aae53aa6789f86f94ab5ec5c' => 
+    array (
+      0 => '/var/www/PHP/6_Calculator_Smarty_OOP_action/app/calc/CalcView.html',
+      1 => 1710616944,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_65f5f45a471e98_58952049 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_24028750665f5f45a4653a1_41820842', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_22444103465f5f45a471351_84974080', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender(($_smarty_tpl->tpl_vars['conf']->value->root_path).("/templates/main.html"), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, true);
+}
+/* {block 'footer'} */
+class Block_24028750665f5f45a4653a1_41820842 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+footer example<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_22444103465f5f45a471351_84974080 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2>Credit calculator - test</h2>
+
+<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+calcCompute" method="post">
+	<fieldset>
+		<label for="credit">Credit value</label>
+		<input id="credit" type="text" placeholder="credit val" name="credit" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->credit;?>
+">
+
+		<label for="percent">Percent value</label>
+		<input id="percent" type="text" placeholder="percent val" name="percent" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->percent;?>
+">
+
+		<label for="years">Years</label>
+		<input id="years" type="text" placeholder="years" name="years" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->years;?>
+">
+	</fieldset>
+	<button type="submit" class="pure-button pure-button-primary">Count</button>
+</form>
+
+<div class="messages">
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+	<h4>Errors: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+	<h4>Information: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->result)) {?>
+	<h4>Result: </h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['res']->value->result;?>
+
+	</p>
+<?php }?>
+
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
