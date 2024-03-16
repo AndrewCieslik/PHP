cb2ebd9ddeac02d9df46cbe4ee2e1033c5c743ab
Andrew <cieslik.andrew@gmail.com>
2024-03-16 08:40:05 +0100

    switcher

--- 6_Calculator_Smarty_OOP_action/app/calc.php
+++ 6_Calculator_Smarty_OOP_action/app/calc.php
@@ -3,5 +3,28 @@ require_once dirname(__FILE__).'/../config.php';
 
 action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
 
-$ctrl = new CalcCtrl();
-$ctrl->process();
+switch ($action) {
+	default : // 'calcView'
+	    // załaduj definicję kontrolera
+		include_once $conf->root_path.'/app/calc/CalcCtrl.class.php';
+		// utwórz obiekt i uzyj
+		$ctrl = new CalcCtrl ();
+		$ctrl->generateView ();
+	break;
+	case 'calcCompute' :
+		// załaduj definicję kontrolera
+		include_once $conf->root_path.'/app/calc/CalcCtrl.class.php';
+		// utwórz obiekt i uzyj
+		$ctrl = new CalcCtrl ();
+		$ctrl->process ();
+	break;
+	case 'action1' :
+		// zrób coś innego ...
+	break;
+	case 'action2' :
+		// zrób coś innego ...
+	break;
+}
+
+// $ctrl = new CalcCtrl();
+// $ctrl->process();
