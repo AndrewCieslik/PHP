7c233cc1e1c6e4606e1fda3097fe7855f072a949
cieslik.andrew <cieslik.andrew@gmail.com>
2024-03-17 08:10:23 +0100

    ctrl.php

--- 6_Domainer_Temp_Smarty_OOP_action/app/calc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/../config.php';
-require_once $conf->root_path. '/lib/smarty/Smarty.class.php';
-
-//załaduj kontroler
-require_once $conf->root_path.'/app/CalcCtrl.class.php';
-
-$ctrl = new CalcCtrl();
-$ctrl->process();
