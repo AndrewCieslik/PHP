135c571981589aa7c328541aac6d2671fadef833
cieslik.andrew <cieslik.andrew@gmail.com>
2024-03-20 12:49:03 +0100

    helloworld works

--- Laravel_Shop/app/Http/Controllers/HelloWorldController.php
+++ Laravel_Shop/app/Http/Controllers/HelloWorldController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HelloWorldController extends Controller
 {
     public function show() {
-        return view('Hello');
+        return view('helloworld');
     }
 }
