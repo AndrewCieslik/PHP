7b2fce85f8bd9b5550ae80873ff2fa7176247c22
cieslik.andrew <cieslik.andrew@gmail.com>
2024-03-20 12:44:45 +0100

    use App\Http\Controllers\HelloWorldController;

--- Laravel_Shop/routes/web.php
+++ Laravel_Shop/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloWorldController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', show() {
-    return view('hello');
-});
+Route::get('/hello', [HelloWorldController::class, 'show']);
