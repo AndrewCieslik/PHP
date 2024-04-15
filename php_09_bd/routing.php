0f76a05751d72a55ddf90acf51b93f15e3c0dd0b
cieslik.andrew <cieslik.andrew@gmail.com>
2024-04-15 13:26:48 +0200

    setDefaultRoute('loginShow')

--- php_09_bd/routing.php
+++ php_09_bd/routing.php
@@ -3,7 +3,7 @@
 use core\App;
 use core\Utils;
 
-App::getRouter()->setDefaultRoute('personList'); // akcja/ścieżka domyślna
+App::getRouter()->setDefaultRoute('loginShow'); // akcja/ścieżka domyślna
 App::getRouter()->setLoginRoute('login'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
 
 Utils::addRoute('personList',    'PersonListCtrl');
