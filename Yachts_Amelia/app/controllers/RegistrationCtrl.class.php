a753c6544ff43d7fe0d56e2068f49daa30e814d5
cieslik.andrew <cieslik.andrew@gmail.com>
2024-04-19 17:13:19 +0200

    registr works

--- Yachts_Amelia/app/controllers/RegistrationCtrl.class.php
+++ Yachts_Amelia/app/controllers/RegistrationCtrl.class.php
@@ -17,13 +17,13 @@ class RegistrationCtrl {
 
     public function validateSave() {
         // Pobranie parametrów z formularza 
-        $this->form->id_user = ParamUtils::getFromRequest('id_user', true, 'Błędne wywołanie aplikacji');
-        $this->form->name = ParamUtils::getFromRequest('name', true, 'Błędne wywołanie aplikacji');
-        $this->form->surname = ParamUtils::getFromRequest('surname', true, 'Błędne wywołanie aplikacji');
-        $this->form->login = ParamUtils::getFromRequest('login', true, 'Błędne wywołanie aplikacji');
-        $this->form->password = ParamUtils::getFromRequest('password', true, 'Błędne wywołanie aplikacji');
-        $this->form->password2 = ParamUtils::getFromRequest('password2', true, 'Błędne wywołanie aplikacji');
-        $this->form->phone = ParamUtils::getFromRequest('phone', true, 'Błędne wywołanie aplikacji');
+        $this->form->id_user = ParamUtils::getFromRequest('id_user');
+        $this->form->name = ParamUtils::getFromRequest('name');
+        $this->form->surname = ParamUtils::getFromRequest('surname');
+        $this->form->login = ParamUtils::getFromRequest('login');
+        $this->form->password = ParamUtils::getFromRequest('password');
+        $this->form->password2 = ParamUtils::getFromRequest('password2');
+        $this->form->phone = ParamUtils::getFromRequest('phone');
 
         // Walidacja danych
         if (empty($this->form->name) || empty($this->form->surname) || empty($this->form->login) || empty($this->form->password)  || empty($this->form->password2) || empty($this->form->phone)) {
@@ -46,55 +46,29 @@ class RegistrationCtrl {
         if ($this->validateSave()) {
             // 2. Zapis danych w bazie
             try {
+                // Wstawienie danych użytkownika do tabeli 'users'
+                App::getDB()->insert("users", [
+                    "name" => $this->form->name,
+                    "surname" => $this->form->surname,
+                    "phone" => $this->form->phone
+                ]);
 
-                //2.1 Nowy rekord
-                if ($this->form->id_yacht == '') {
-                    //sprawdź liczebność rekordów - nie pozwalaj przekroczyć 20
-                    $count = App::getDB()->count("yachts");
-                    if ($count <= 20) {
-                        App::getDB()->insert("users", [
-                            "name" => $this->form->name,
-                            "surname" => $this->form->surname,
-                            "phone" => $this->form->phone
-                        ]);
-                        App::getDB()->insert("passwords", [
-                            "login" => $this->form->login,
-                            "password" => $this->form->password,
-                            "id_user" => $this->form->id_user
-                        ]);
-                    } else { //za dużo rekordów
-                        // Gdy za dużo rekordów to pozostań na stronie
-                        Utils::addInfoMessage('Ograniczenie: Zbyt dużo rekordów. Aby dodać nowy usuń wybrany wpis.');
-                        $this->generateView(); //pozostań na stronie edycji
-                        exit(); //zakończ przetwarzanie, aby nie dodać wiadomości o pomyślnym zapisie danych
-                    }
-                } else {
-                    //2.2 Edycja rekordu o danym ID
-                    App::getDB()->update("users", [
-                        "name" => $this->form->name,
-                        "surname" => $this->form->surname,
-                        "phone" => $this->form->phone
-                            ], [
-                        "id_user" => $this->form->id_user
-                    ]);
-                    App::getDB()->update("passwords", [
-                        "login" => $this->form->login,
-                        "password" => $this->form->password
-                            ], [
-                        "id_user" => $this->form->id_user
-                    ]);
-                }
-                Utils::addInfoMessage('Pomyślnie zapisano rekord');
+                // Wstawienie danych hasła do tabeli 'passwords'
+                App::getDB()->insert("passwords", [
+                    "login" => $this->form->login,
+                    "password" => $this->form->password,
+                    "id_user" => App::getDB()->id() //Ostatnie użyte id_users
+                ]);
             } catch (\PDOException $e) {
                 Utils::addErrorMessage('Wystąpił nieoczekiwany błąd podczas zapisu rekordu');
                 if (App::getConf()->debug)
                     Utils::addErrorMessage($e->getMessage());
             }
 
-            // 3b. Po zapisie przejdź na stronę listy osób (w ramach tego samego żądania http)
-            App::getRouter()->forwardTo('personList');
+            // Przekierowanie na inną stronę po zapisaniu danych
+            App::getRouter()->forwardTo('login');
         } else {
-            // 3c. Gdy błąd walidacji to pozostań na stronie
+            // Pozostanie na stronie w przypadku błędów walidacji
             $this->generateView();
         }
     }
@@ -104,5 +78,10 @@ class RegistrationCtrl {
         App::getSmarty()->assign('form', $this->form);
         // Wyświetlenie widoku
         App::getSmarty()->display('RegistrationView.tpl');
+    }
+
+    public function id() {
+        // Pobranie ostatnio nadanego ID z bazy danych (MySQL i PDO)
+        return App::getDB()->lastInsertId();
     }
 }
