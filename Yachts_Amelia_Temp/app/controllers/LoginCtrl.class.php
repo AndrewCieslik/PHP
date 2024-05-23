17bb5b36fd978d552da4a2f39de24ab83f793ef8
andrew <cieslik.andrew@gmail.com>
2024-05-23 13:22:55 +0200

    check role = not working

--- Yachts_Amelia_Temp/app/controllers/LoginCtrl.class.php
+++ Yachts_Amelia_Temp/app/controllers/LoginCtrl.class.php
@@ -18,37 +18,61 @@ class LoginCtrl {
     }
 
     public function validate() {
+        //1. mam login i hasło z formularza
         $this->form->login = ParamUtils::getFromRequest('login');
         $this->form->pass = ParamUtils::getFromRequest('pass');
 
-        //nie ma sensu walidować dalej, gdy brak parametrów
-        if (!isset($this->form->login))
+        if (!isset($this->form->login)){
             return false;
-
-        // sprawdzenie, czy potrzebne wartości zostały przekazane
+        }
         if (empty($this->form->login)) {
             Utils::addErrorMessage('Nie podano loginu');
         }
         if (empty($this->form->pass)) {
             Utils::addErrorMessage('Nie podano hasła');
         }
-
-        //nie ma sensu walidować dalej, gdy brak wartości
         if (App::getMessages()->isError()){
             return false;
         }
+        //4. sprawdz nazwe roli dla tego id_role i dodaj addRole
 
+        //2. znajdz id_user w bazie dla tego loginu
         $this->form->db_id_user = App::getDB()->get("users", "id_user", [
-            "login" => $this->form->login  //warunek przypisania
+            "login" => $this->form->login  //if
         ]);
+
+        //3. znajdz i porownaj hasło z bazy z hasłem z formularza
         if (!$this->form->db_id_user) {
             Utils::addErrorMessage('Użytkownik o podanym loginie nie istnieje');
         }
         $this->form->db_password = App::getDB()->get("passwords", "password", [
-            "id_user" => $this->form->db_id_user  //warunek przypisania
+            "id_user" => $this->form->db_id_user
         ]);
+        if (!$this->form->db_password) {
+            Utils::addErrorMessage('Brak hasła użytkownika w bazie');
+        }
+        //4. jeśli hasło z formularza jest takie samo, jak w tablicy passwords
         if ($this->form->pass == $this->form->db_password) {
+            Utils::addErrorMessage('Prawidłowe hasło');
+        } else {
+            Utils::addErrorMessage('Nieprawidłowe hasło');
+            return false;
+        }
+        //5. to jakie id_role ma przypisany użytkownik (wg id_user) w tablicy assigned_roles
+        $this->form->db_id_role = App::getDB()->get("assigned_roles", "id_role", [
+            "id_user" => $this->form->db_id_user
+        ]);
+        //6. sprawdź nazwę roli dla takiego id_role w tablicy roles
+        $this->form->db_role = App::getDB()->get("roles", "role", [
+            "id_role" => $this->form->db_id_role
+        ]);
+        //7. przypisz tą nazwę roli użytkownikowi
+        if (!$this->form->db_role) {
             RoleUtils::addRole('user');
+        } else {
+            RoleUtils::addRole($this->form->db_role);
+            RoleUtils::addRole('user');
+
         }
 
 //        if (!$this->form->db_login) {
@@ -60,13 +84,13 @@ class LoginCtrl {
 //            return false;
 //        }
 
-        if ($this->form->login == "marta" && $this->form->pass == "marta") {
-            //RoleUtils::addRole('admin');
-        } else if ($this->form->login == "user" && $this->form->pass == "user") {
-           // RoleUtils::addRole('user');
-        } else {
-            Utils::addErrorMessage('Niepoprawny login lub hasło');
-        }
+//        if ($this->form->login == "marta" && $this->form->pass == "marta") {
+//            //RoleUtils::addRole('admin');
+//        } else if ($this->form->login == "user" && $this->form->pass == "user") {
+//           // RoleUtils::addRole('user');
+//        } else {
+//            Utils::addErrorMessage('Niepoprawny login lub hasło');
+//        }
         return !App::getMessages()->isError();
     }
 
--- Yachts_Amelia_Temp/app/forms/LoginForm.class.php
+++ Yachts_Amelia_Temp/app/forms/LoginForm.class.php
@@ -6,6 +6,7 @@ class LoginForm {
 	public $login;
 	public $pass;
     public $db_id_user;
-    public $db_login;
     public $db_password;
+    public $db_id_role;
+    public $db_role;
 }
