5adba072ca995ba63a6b897323495611b50e0600
andrew <cieslik.andrew@gmail.com>
2024-05-31 20:53:37 +0200

    see ID when move on name/surname

--- Yachts_Amelia_Temp/app/views/CharterList.tpl
+++ Yachts_Amelia_Temp/app/views/CharterList.tpl
@@ -1,95 +1,89 @@
 {extends file="main.tpl"}
 
 {block name=top}
-	<div class="bottom-margin">
-	<form class="pure-form pure-form-stacked" action="{$conf->action_url}charterList">
-		<legend>Opcje wyszukiwania</legend>
-		<fieldset>
-			<input type="text" placeholder="numer charteru" name="sf_charter" value="{$searchForm->id_charter}" /><br />
-			<button type="submit" class="pure-button pure-button-primary">Filtruj</button>
-		</fieldset>
-	</form>
-	</div>
-	<div class="bottom-margin">
-		<a class="pure-button button-success" href="{$conf->action_root}charterNew">+ Nowy charter</a>
-	</div>
+    <div class="bottom-margin">
+        <form class="pure-form pure-form-stacked" action="{$conf->action_url}charterList">
+            <legend>Opcje wyszukiwania</legend>
+            <fieldset>
+                <input type="text" placeholder="numer charteru" name="sf_charter" value="{$searchForm->id_charter}" /><br />
+                <button type="submit" class="pure-button pure-button-primary">Filtruj</button>
+            </fieldset>
+        </form>
+    </div>
+    <div class="bottom-margin">
+        <a class="pure-button button-success" href="{$conf->action_root}charterNew">+ Nowy charter</a>
+    </div>
 {/block}
 
 {block name=bottom}
 
-	<table id="tab_people" class="pure-table pure-table-bordered">
-	<thead>
-		<tr>
-			{if core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
-				<th>id klienta</th>
-			{/if}
-				<th>nazwa jachtu</th>
-				<th>data rozpoczecia</th>
-				<th>data zakonczenia</th>
-			{if core\RoleUtils::inRole("user") or core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
-				<th>imie sternika</th>
-				<th>nazwisko</th>
-			{/if}
-			{if core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
-				<th>zatwierdz [0/1]</th>
-			{/if}
-			{if core\RoleUtils::inRole("user") or core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
-				<th>opcje</th>
-			{/if}
-
-		</tr>
-	</thead>
-	<tbody>
-	{if !core\RoleUtils::inRole("user") and !core\RoleUtils::inRole("manager") and !core\RoleUtils::inRole("admin")}
-		{foreach $charters as $p}
-				{strip}
-					<tr>
-						<td>{$p["yacht_name"]}</td>
-						<td>{$p["date_start"]}</td>
-						<td>{$p["date_end"]}</td>
-					</tr>
-				{/strip}
-		{/foreach}
-	{/if}
-	{if core\RoleUtils::inRole("user")}
-		{foreach $charters as $p}
-			{if $p["id_user"] == {$_SESSION['id']}}
-				{strip}
-					<tr>
-						<td>{$p["id_user"]}</td>
-						<td>{$p["yacht_name"]}</td>
-						<td>{$p["date_start"]}</td>
-						<td>{$p["date_end"]}</td>
-						<td>{$p["name"]}</td>
-						<td>{$p["surname"]}</td>
-						<td>
-							<a class="button-small pure-button button-secondary" href="{$conf->action_url}charterEdit/{$p['id_charter']}">Edytuj</a>
-							<a class="button-small pure-button button-warning" href="{$conf->action_url}charterDelete/{$p['id_charter']}">Usuń</a>
-						</td>
-					</tr>
-				{/strip}
-			{/if}
-		{/foreach}
-	{/if}
-	{if core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
-		{foreach $charters as $p}
-			{strip}
-				<tr>
-					<td>{$p["id_user"]}</td>
-					<td>{$p["yacht_name"]}</td>
-					<td>{$p["date_start"]}</td>
-					<td>{$p["date_end"]}</td>
-					<td>{$p["name"]}</td>
-					<td>{$p["surname"]}</td>
-					<td>{$p["approved"]}</td>
-					<td>
-						<a class="button-small pure-button button-secondary" href="{$conf->action_url}charterEdit/{$p['id_charter']}">Edytuj</a>
-						<a class="button-small pure-button button-warning" href="{$conf->action_url}charterDelete/{$p['id_charter']}">Usuń</a>
-					</td>
-				</tr>
-			{/strip}
-		{/foreach}
-	{/if}
-	</tbody>
-	</table>
+    <table id="tab_people" class="pure-table pure-table-bordered">
+        <thead>
+        <tr>
+            <th>nazwa jachtu</th>
+            <th>data rozpoczecia</th>
+            <th>data zakonczenia</th>
+            {if core\RoleUtils::inRole("user") or core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
+                <th>imie sternika</th>
+                <th>nazwisko</th>
+            {/if}
+            {if core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
+                <th>zatwierdz [0/1]</th>
+            {/if}
+            {if core\RoleUtils::inRole("user") or core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
+                <th>opcje</th>
+            {/if}
+        </tr>
+        </thead>
+        <tbody>
+        {if !core\RoleUtils::inRole("user") and !core\RoleUtils::inRole("manager") and !core\RoleUtils::inRole("admin")}
+            {foreach $charters as $p}
+                {strip}
+                    <tr>
+                        <td>{$p["yacht_name"]}</td>
+                        <td>{$p["date_start"]}</td>
+                        <td>{$p["date_end"]}</td>
+                    </tr>
+                {/strip}
+            {/foreach}
+        {/if}
+        {if core\RoleUtils::inRole("user")}
+            {foreach $charters as $p}
+                {if $p["id_user"] == {$_SESSION['id']}}
+                    {strip}
+                        <tr>
+                            <td>{$p["yacht_name"]}</td>
+                            <td>{$p["date_start"]}</td>
+                            <td>{$p["date_end"]}</td>
+                            <td title="ID: {$p["id_user"]}">{$p["name"]}</td>
+                            <td title="ID: {$p["id_user"]}">{$p["surname"]}</td>
+                            <td>
+                                <a class="button-small pure-button button-secondary" href="{$conf->action_url}charterEdit/{$p['id_charter']}">Edytuj</a>
+                                <a class="button-small pure-button button-warning" href="{$conf->action_url}charterDelete/{$p['id_charter']}">Usuń</a>
+                            </td>
+                        </tr>
+                    {/strip}
+                {/if}
+            {/foreach}
+        {/if}
+        {if core\RoleUtils::inRole("manager") or core\RoleUtils::inRole("admin")}
+            {foreach $charters as $p}
+                {strip}
+                    <tr>
+                        <td>{$p["yacht_name"]}</td>
+                        <td>{$p["date_start"]}</td>
+                        <td>{$p["date_end"]}</td>
+                        <td title="ID: {$p["id_user"]}">{$p["name"]}</td>
+                        <td title="ID: {$p["id_user"]}">{$p["surname"]}</td>
+                        <td>{$p["approved"]}</td>
+                        <td>
+                            <a class="button-small pure-button button-secondary" href="{$conf->action_url}charterEdit/{$p['id_charter']}">Edytuj</a>
+                            <a class="button-small pure-button button-warning" href="{$conf->action_url}charterDelete/{$p['id_charter']}">Usuń</a>
+                        </td>
+                    </tr>
+                {/strip}
+            {/foreach}
+        {/if}
+        </tbody>
+    </table>
 {/block}
--- Yachts_Amelia_Temp/public/templates_c/2f5a0fae437f870f2a4534d18d9d76f53be793ef_0.file.CharterList.tpl.php
+++ Yachts_Amelia_Temp/public/templates_c/2f5a0fae437f870f2a4534d18d9d76f53be793ef_0.file.CharterList.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2024-05-30 09:50:07
+/* Smarty version 3.1.30, created on 2024-05-31 20:50:31
   from "C:\xampp\htdocs\PHP\Yachts_Amelia_Temp\app\views\CharterList.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_66582fafb56280_81902618',
+  'unifunc' => 'content_665a1bf744d0b9_52926507',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2f5a0fae437f870f2a4534d18d9d76f53be793ef' => 
     array (
       0 => 'C:\\xampp\\htdocs\\PHP\\Yachts_Amelia_Temp\\app\\views\\CharterList.tpl',
-      1 => 1717055406,
+      1 => 1717181427,
       2 => 'file',
     ),
   ),
@@ -21,152 +21,148 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:main.tpl' => 1,
   ),
 ),false)) {
-function content_66582fafb56280_81902618 (Smarty_Internal_Template $_smarty_tpl) {
+function content_665a1bf744d0b9_52926507 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_211853670866582fafb12f77_42843379', 'top');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_552285568665a1bf7407915_57572494', 'top');
 ?>
 
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_144412444266582fafb54dd2_71680409', 'bottom');
-?>
-
-<?php $_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_611268131665a1bf744c3c0_37170800', 'bottom');
+$_smarty_tpl->inheritance->endChild();
 $_smarty_tpl->_subTemplateRender("file:main.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
 }
 /* {block 'top'} */
-class Block_211853670866582fafb12f77_42843379 extends Smarty_Internal_Block
+class Block_552285568665a1bf7407915_57572494 extends Smarty_Internal_Block
 {
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
-	<div class="bottom-margin">
-	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+    <div class="bottom-margin">
+        <form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 charterList">
-		<legend>Opcje wyszukiwania</legend>
-		<fieldset>
-			<input type="text" placeholder="numer charteru" name="sf_charter" value="<?php echo $_smarty_tpl->tpl_vars['searchForm']->value->id_charter;?>
+            <legend>Opcje wyszukiwania</legend>
+            <fieldset>
+                <input type="text" placeholder="numer charteru" name="sf_charter" value="<?php echo $_smarty_tpl->tpl_vars['searchForm']->value->id_charter;?>
 " /><br />
-			<button type="submit" class="pure-button pure-button-primary">Filtruj</button>
-		</fieldset>
-	</form>
-	</div>
-	<div class="bottom-margin">
-		<a class="pure-button button-success" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+                <button type="submit" class="pure-button pure-button-primary">Filtruj</button>
+            </fieldset>
+        </form>
+    </div>
+    <div class="bottom-margin">
+        <a class="pure-button button-success" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
 charterNew">+ Nowy charter</a>
-	</div>
+    </div>
 <?php
 }
 }
 /* {/block 'top'} */
 /* {block 'bottom'} */
-class Block_144412444266582fafb54dd2_71680409 extends Smarty_Internal_Block
+class Block_611268131665a1bf744c3c0_37170800 extends Smarty_Internal_Block
 {
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 
-	<table id="tab_people" class="pure-table pure-table-bordered">
-	<thead>
-		<tr>
-			<?php if (core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
-				<th>id klienta</th>
-			<?php }?>
-				<th>nazwa jachtu</th>
-				<th>data rozpoczecia</th>
-				<th>data zakonczenia</th>
-			<?php if (core\RoleUtils::inRole("user") || core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
-				<th>imie sternika</th>
-				<th>nazwisko</th>
-			<?php }?>
-			<?php if (core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
-				<th>zatwierdz [0/1]</th>
-			<?php }?>
-			<?php if (core\RoleUtils::inRole("user") || core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
-				<th>opcje</th>
-			<?php }?>
-
-		</tr>
-	</thead>
-	<tbody>
-	<?php if (!core\RoleUtils::inRole("user") && !core\RoleUtils::inRole("manager") && !core\RoleUtils::inRole("admin")) {?>
-		<?php
+    <table id="tab_people" class="pure-table pure-table-bordered">
+        <thead>
+        <tr>
+            <th>nazwa jachtu</th>
+            <th>data rozpoczecia</th>
+            <th>data zakonczenia</th>
+            <?php if (core\RoleUtils::inRole("user") || core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
+                <th>imie sternika</th>
+                <th>nazwisko</th>
+            <?php }?>
+            <?php if (core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
+                <th>zatwierdz [0/1]</th>
+            <?php }?>
+            <?php if (core\RoleUtils::inRole("user") || core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
+                <th>opcje</th>
+            <?php }?>
+        </tr>
+        </thead>
+        <tbody>
+        <?php if (!core\RoleUtils::inRole("user") && !core\RoleUtils::inRole("manager") && !core\RoleUtils::inRole("admin")) {?>
+            <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['charters']->value, 'p');
 if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['p']->value) {
 ?>
-				<tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["yacht_name"];?>
+                <tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["yacht_name"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["date_start"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["date_end"];?>
 </td></tr>
-		<?php
+            <?php
 }
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 ?>
 
-	<?php }?>
-	<?php if (core\RoleUtils::inRole("user")) {?>
-		<?php
+        <?php }?>
+        <?php if (core\RoleUtils::inRole("user")) {?>
+            <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['charters']->value, 'p');
 if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['p']->value) {
 ?>
-			<?php ob_start();
+                <?php ob_start();
 echo $_smarty_tpl->tpl_vars['_SESSION']->value['id'];
 $_prefixVariable1=ob_get_clean();
 if ($_smarty_tpl->tpl_vars['p']->value["id_user"] == $_prefixVariable1) {?>
-				<tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["id_user"];?>
-</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["yacht_name"];?>
+                    <tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["yacht_name"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["date_start"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["date_end"];?>
-</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["name"];?>
-</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["surname"];?>
+</td><td title="ID: <?php echo $_smarty_tpl->tpl_vars['p']->value["id_user"];?>
+"><?php echo $_smarty_tpl->tpl_vars['p']->value["name"];?>
+</td><td title="ID: <?php echo $_smarty_tpl->tpl_vars['p']->value["id_user"];?>
+"><?php echo $_smarty_tpl->tpl_vars['p']->value["surname"];?>
 </td><td><a class="button-small pure-button button-secondary" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 charterEdit/<?php echo $_smarty_tpl->tpl_vars['p']->value['id_charter'];?>
 ">Edytuj</a><a class="button-small pure-button button-warning" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 charterDelete/<?php echo $_smarty_tpl->tpl_vars['p']->value['id_charter'];?>
 ">Usuń</a></td></tr>
-			<?php }?>
-		<?php
+                <?php }?>
+            <?php
 }
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 ?>
 
-	<?php }?>
-	<?php if (core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
-		<?php
+        <?php }?>
+        <?php if (core\RoleUtils::inRole("manager") || core\RoleUtils::inRole("admin")) {?>
+            <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['charters']->value, 'p');
 if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['p']->value) {
 ?>
-			<tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["id_user"];?>
-</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["yacht_name"];?>
+                <tr><td><?php echo $_smarty_tpl->tpl_vars['p']->value["yacht_name"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["date_start"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["date_end"];?>
-</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["name"];?>
-</td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["surname"];?>
+</td><td title="ID: <?php echo $_smarty_tpl->tpl_vars['p']->value["id_user"];?>
+"><?php echo $_smarty_tpl->tpl_vars['p']->value["name"];?>
+</td><td title="ID: <?php echo $_smarty_tpl->tpl_vars['p']->value["id_user"];?>
+"><?php echo $_smarty_tpl->tpl_vars['p']->value["surname"];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['p']->value["approved"];?>
 </td><td><a class="button-small pure-button button-secondary" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 charterEdit/<?php echo $_smarty_tpl->tpl_vars['p']->value['id_charter'];?>
 ">Edytuj</a><a class="button-small pure-button button-warning" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 charterDelete/<?php echo $_smarty_tpl->tpl_vars['p']->value['id_charter'];?>
 ">Usuń</a></td></tr>
-		<?php
+            <?php
 }
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 ?>
 
-	<?php }?>
-	</tbody>
-	</table>
+        <?php }?>
+        </tbody>
+    </table>
 <?php
 }
 }
